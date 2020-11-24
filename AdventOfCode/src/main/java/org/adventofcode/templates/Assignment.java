ea8634c76b0c0f0d78fdfbb9a7964db229f01236
Magnus Jensen <magnushjensen.mail@gmail.com>
2020-11-24 08:59:38 +0100

    Added method, to load file input into the input field.

--- AdventOfCode/src/main/java/org/adventofcode/templates/Assignment.java
+++ AdventOfCode/src/main/java/org/adventofcode/templates/Assignment.java
@@ -1,16 +1,14 @@
 package org.adventofcode.templates;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
+
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.TextArea;
 import org.adventofcode.App;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.nio.file.Path;
+import java.util.Scanner;
 
 public abstract class Assignment {
 
@@ -32,10 +30,20 @@ public abstract class Assignment {
         return loader.load();
     }
 
-    protected Node loadDefaultContent (Object controller) throws IOException {
+    protected Node loadDefaultContent (Assignment controller) throws IOException {
         FXMLLoader loader = new FXMLLoader(App.class.getResource("defaultContent.fxml"));
         loader.setController(controller);
         return loader.load();
+    }
+
+    protected void setInputContent(TextArea input, int assignmentNumber) throws IOException {
+        Path inputPath = Path.of("./input", assignmentNumber + ".txt");
+        if (inputPath.toFile().exists()) {
+            Scanner scanner = new Scanner(Path.of("./input", assignmentNumber + ".txt"));
+            while (scanner.hasNextLine()) {
+                input.appendText(scanner.nextLine() + "\n");
+            }
+        }
     }
 
     @Override
