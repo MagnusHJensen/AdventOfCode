8e4a6c4a8214adafc845eadc683203f328d7af9c
Magnus Jensen <magnushjensen.mail@gmail.com>
2021-12-04 20:20:39 +0100

    day 4

---
+++ AdventOfCode/src/main/java/org/adventofcode/cal2021/A4P1.java
@@ -0,0 +1,137 @@
+package org.adventofcode.cal2021;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.TextArea;
+import org.adventofcode.templates.Assignment;
+import org.adventofcode.templates.CalenderAssignment;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@CalenderAssignment(calendarName = "2021", assignmentName = "Giant Squid", number = 7, description = "Placeholder.")
+public class A4P1 extends Assignment {
+	@FXML
+	private TextArea input;
+	@FXML
+	private TextArea output;
+
+	public A4P1(String name) {
+		super(name);
+	}
+
+	@Override
+	public Node getContent() throws IOException {
+		Node content = loadDefaultContent(this);
+		setInputContent(input, 2021, 4);
+		return content;
+	}
+
+	public void run(ActionEvent actionEvent) {
+		String[] lines = input.getText().split("\n");
+
+		String[] numbersToDraw = lines[0].split(",");
+		ArrayList<int[][]> boards = new ArrayList<>();
+		ArrayList<boolean[][]> filledSlots = new ArrayList<>();
+		lines = Arrays.copyOfRange(lines, 2, lines.length);
+
+
+		// initialize boards
+		for (int board = 0; board < lines.length; board += 6) {
+			int[][] currentBoard = new int[5][5];
+			boolean[][] slots = new boolean[5][5];
+			for (int x = 0; x < 5; x++) {
+				Arrays.fill(slots[x], false);
+				String[] row = lines[board + x].split(" ");
+				int blankSpaces = 0;
+				for (int y = 0; y < row.length; y++) {
+
+					if (row[y].equals("")) {
+						blankSpaces++;
+						continue;
+					}
+					currentBoard[x][y - blankSpaces] = Integer.parseInt(row[y]);
+				}
+			}
+			boards.add(currentBoard);
+			filledSlots.add(slots);
+		}
+
+		int winnerBoardIdx = -1;
+		int winnerNumber = -1;
+		// Draw numbers and check for winner.
+		for (String stringNum : numbersToDraw) {
+			if (winnerBoardIdx > -1) break;
+			int number = Integer.parseInt(stringNum);
+			// Set slot to true on the board where the number is found.
+			for (int boardIdx = 0; boardIdx < boards.size(); boardIdx++) {
+				int[][] board = boards.get(boardIdx);
+				for (int x = 0; x < board.length; x++) {
+					for (int y = 0; y < board[x].length; y++) {
+						if (board[x][y] == number) {
+							filledSlots.get(boardIdx)[x][y] = true;
+						}
+					}
+				}
+			}
+
+
+			// Check for winner board.
+			for (int boardIdx = 0; boardIdx < boards.size(); boardIdx++) {
+				boolean[][] slotBoard = filledSlots.get(boardIdx);
+
+				for (int x = 0; x < slotBoard.length; x++) {
+
+					// Row checking logic.
+					boolean rowFilled = true;
+					for (int y = 0; y < slotBoard[x].length; y++) {
+
+						// Column checking
+						if (x == 0) {
+							boolean columnFilled = true;
+							if (!slotBoard[x][y] || !slotBoard[x + 1][y] || !slotBoard[x+2][y] || !slotBoard[x+3][y] || !slotBoard[x+4][y]) {
+								columnFilled = false;
+							}
+							if (columnFilled) {
+								winnerBoardIdx = boardIdx;
+								winnerNumber = number;
+								break;
+							}
+						}
+
+						if (!slotBoard[x][y]) {
+							rowFilled = false;
+							break;
+						}
+					}
+					if (rowFilled) {
+						winnerBoardIdx = boardIdx;
+						winnerNumber = number;
+						break;
+					}
+				}
+			}
+
+
+
+		}
+
+		int[][] winnerBoard = boards.get(winnerBoardIdx);
+		boolean[][] winnerBoardSlots = filledSlots.get(winnerBoardIdx);
+
+		int unmarkedSum = 0;
+		for (int x = 0; x < winnerBoard.length; x++) {
+			for (int y = 0; y < winnerBoard[x].length; y++) {
+				if (!winnerBoardSlots[x][y]) {
+					unmarkedSum += winnerBoard[x][y];
+				}
+			}
+		}
+
+
+		output.setText("Board winner: " + (winnerBoardIdx + 1) + "\nWinner number: " + winnerNumber + "\nResult: " + (unmarkedSum * winnerNumber));
+
+	}
+}
