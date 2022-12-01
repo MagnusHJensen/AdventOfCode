6991e5487e146bfa97077c70a2cd87e458b77d69
MagnusHJensen <magnus.holm.jensen@lego.dk>
2022-12-01 17:20:04 +0100

    Add template for day 2 and 3

--- src/main/java/dk/magnusjensen/adventofcode/cal2022/A3P2.java
+++ src/main/java/dk/magnusjensen/adventofcode/cal2022/A3P2.java
@@ -5,6 +5,7 @@ import dk.magnusjensen.adventofcode.templates.CalenderAssignment;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
 import java.io.IOException;
@@ -15,6 +16,8 @@ public class A3P2 extends Assignment {
     private TextArea input;
     @FXML
     private TextArea output;
+    @FXML
+    private Label outputLabel;
 
     public A3P2(String name) {
         super(name);
@@ -23,12 +26,16 @@ public class A3P2 extends Assignment {
     @Override
     public Node getContent() throws IOException {
         Node content = loadDefaultContent(this);
-        setInputContent(input, 2022, 6);
+        setInputContent(input, 2022, 3);
         return content;
     }
 
     public void run(ActionEvent actionEvent) {
+        long start = System.nanoTime();
         String[] lines = input.getText().split("\n");
 
+        long end = System.nanoTime();
+        outputLabel.setText("Output - Execution time: " + (end - start) / 1_000_000_000d);
+        output.setText("Output: ");
     }
 }
